d742d28fd852309556010a9e2f6be9dc39b3b6d2
MOISE Rajesearison <moiseraidjy@gmail.com>
2021-05-11 16:26:53 +0300

    add the main page

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBkA88f70C_N2izq7Ovpf9WJ7Rg6YsDoLQ",
+  authDomain: "netflix-clone-6cfdc.firebaseapp.com",
+  projectId: "netflix-clone-6cfdc",
+  storageBucket: "netflix-clone-6cfdc.appspot.com",
+  messagingSenderId: "353832057576",
+  appId: "1:353832057576:web:a79f4e9ecc0165a4dc675d",
+};
